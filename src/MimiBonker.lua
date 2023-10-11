8e64c3dca05373bb2a7ca8fad003e18954a2a27f
Jayden <Darknytely@gmail.com>
2023-10-11 17:39:19 +0200

    Added more descriptive comments

--- src/MimiBonker.lua
+++ src/MimiBonker.lua
@@ -19,7 +19,7 @@ ItemQualityColors = {
   "FFA335EE", -- Epic (Purple)
   "FFFF8000", -- Legendary (Orange)
   "FFE6CC80", -- Artifact (Gold)
-  "FFFFD864", -- Artifact (Gold)
+  "FFFFD864", -- Vanity / Heirloom (Gold)
 }
 
 -- Define the UI setup function
@@ -27,43 +27,44 @@ function DrawUI()
   -- Create the frame container
   ConfigFrame = AceGUI:Create("Frame")
 
+  -- Set the dimensions and appearance of the frame
   ConfigFrame:SetWidth(400)
   ConfigFrame:SetHeight(500)
-  ConfigFrame:SetTitle("|cFFE6CC80Mimi's Bonk Config|r")
-  ConfigFrame:SetStatusText("Close and Save")
+  ConfigFrame:SetTitle("|cFFE6CC80Mimi's Bonk Config|r") -- Set title with gold text
+  ConfigFrame:SetStatusText("Close and Save") -- Set status text
   ConfigFrame:SetCallback("OnClose", function(widget)
-    AceGUI:Release(widget)
+    AceGUI:Release(widget) -- Release the frame when closed and trigger BagLoop
     BagLoop()
   end)
 
-  ConfigFrame:SetLayout("Fill")
-
-  -- Create the TabGroup
+  ConfigFrame:SetLayout("Fill") -- Set layout to fill the frame
+  -- Create a ScrollFrame (tab container) within the frame
   local tab = AceGUI:Create("ScrollFrame")
 
-  tab:SetLayout("Flow")
-
-  -- Add the TabGroup to the frame container
+  tab:SetLayout("Flow") -- Set the layout to flow
+  -- Add the ScrollFrame (tab container) to the frame container
   ConfigFrame:AddChild(tab)
 
+  -- Populate the first tab (DrawGroup) with checkboxes
   DrawGroup(tab)
 end
 
 -- Function to draw widgets for the first tab
 function DrawGroup(container)
-  container:SetFullWidth(true)
-  container:SetLayout("Flow")
-
+  container:SetFullWidth(true) -- Set the tab to use the full width
+  container:SetLayout("Flow") -- Set layout to flow (left to right)
   -- Create checkboxes for item qualities
   for i = 0, 7 do
     local BoxGroup = AceGUI:Create("SimpleGroup")
 
+    -- Configure the layout of the checkbox group
     BoxGroup:SetLayout("Flow")
     BoxGroup:SetHeight(25)
     BoxGroup:SetFullWidth(true)
 
     local OptionsGroup = AceGUI:Create("SimpleGroup")
 
+    -- Configure the layout of the options group
     OptionsGroup:SetLayout("Flow")
     OptionsGroup:SetFullWidth(true)
     OptionsGroup:SetHeight(30)
@@ -71,37 +72,39 @@ function DrawGroup(container)
     local DestroyBox = AceGUI:Create("CheckBox")
 
     DestroyBox:SetWidth(100)
-    DestroyBox:SetLabel("|cFFFF0000Destroy|r")
-
+    DestroyBox:SetLabel("|cFFFF0000Destroy|r") -- Set label text with red color
     local SellBox = AceGUI:Create("CheckBox")
 
     SellBox:SetWidth(50)
-    SellBox:SetLabel("|cFFE6CC80Sell|r")
-
+    SellBox:SetLabel("|cFFE6CC80Sell|r") -- Set label text with gold color
     local header = AceGUI:Create("Label")
 
-    header:SetFont("Fonts\\MORPHEUS.TTF", 14)
-    header:SetText("|c" .. ItemQualityColors[i + 1] .. ItemQualities[i] .. "|r")
+    header:SetFont("Fonts\\MORPHEUS.TTF", 14) -- Set the font and size
+    header:SetText("|c" .. ItemQualityColors[i + 1] .. ItemQualities[i] .. "|r") -- Set text with item quality color
     if LocalVar.DestroyQualities[i] then
       DestroyBox:SetValue(LocalVar.DestroyQualities[i])
     end
 
+    -- Define callbacks for checkbox changes
     DestroyBox:SetCallback("OnValueChanged", function(self, event, value)
       LocalVar.DestroyQualities[i] = value
-      SellBox:SetValue(false)
+      SellBox:SetValue(false) -- If destroying, uncheck selling
     end)
 
     if LocalVar.SellQualities[i] then
       SellBox:SetValue(LocalVar.SellQualities[i])
     end
+
+    -- Define callbacks for checkbox changes
     SellBox:SetCallback("OnValueChanged", function(self, event, value)
       LocalVar.SellQualities[i] = value
 
       if value == true then
-        DestroyBox:SetValue(false)
+        DestroyBox.SetValue(false) -- If selling, uncheck destroying
       end
     end)
 
+    -- Add the components to the tab
     OptionsGroup:AddChild(SellBox)
     OptionsGroup:AddChild(DestroyBox)
     BoxGroup:AddChild(header)
@@ -131,6 +134,8 @@ function purgeInventory(itemName, rarity, bag, item)
   PickupContainerItem(bag, item)
   DeleteCursorItem()
 end
+
+-- Function to loop through bags and perform actions
 function BagLoop()
   for bag = 0, 4 do
     if GetBagName(bag) then
@@ -144,12 +149,15 @@ function BagLoop()
           if LocalVar.DestroyQualities[itemRarity] == true then
             purgeInventory(itemName, itemRarity, bag, item)
           end
-          if LocalVar.SellQualities[itemRarity] == true then  end
+          if LocalVar.SellQualities[itemRarity] == true then
+            -- Perform selling action
+          end
         end
       end
     end
   end
 end
+
 -- Event handler function
 function EventHandler(self, event, arg)
   if event == "ADDON_LOADED" and arg == "MimiBonker" then
